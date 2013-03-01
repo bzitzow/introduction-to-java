cd208c456fff56fc49efe6f90d8fbb269c5db50c
Brian Zitzow <bzitzow@gmail.com>
2013-03-01 02:41:31 -0800

    Week 4 FindNearestPoints

---
+++ week4/FindNearestPoints.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bzitzow
+ * Date: 2/28/13
+ * Time: 6:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FindNearestPoints {
+}
